aca36b81eaade089d91706374f761699dbd2b812
Mayank Chauhan <mayankrchauhan534@gmail.com>
2024-06-12 12:14:11 +0530

    Preparing for deploy

--- src/index.js
+++ src/index.js
@@ -25,7 +25,6 @@ const router = createBrowserRouter(
       <Route path="/commercial" element={<Properties purpose="COMMERCIAL" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      {/* <Route path="/pdetails/query" element={<PropertyDetails />} /> */}
     </Route>
   )
 );

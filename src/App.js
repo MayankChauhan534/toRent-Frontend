8b2793a73673b25f4f5345864d195ead598b22ae
Mayank Chauhan <mayankrchauhan534@gmail.com>
2024-06-12 12:09:07 +0530

    Preparing for deploy

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <header>
-        <Navbar />
-      </header> */}
       <main>
         <Outlet />
       </main>
